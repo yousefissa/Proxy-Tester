b877b49a3ae70316aa008b127081b0d9dd5f33d4
yomo710 <yomo710@gmail.com>
2017-02-10 03:18:44 -0600

    Accepts user:pass authentication

--- proxy_tester.py
+++ proxy_tester.py
@@ -8,8 +8,7 @@ session = Session()
 session.headers.update({
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36'
     '(KHTML, like Gecko) Chrome/56.0.2924.28 Safari/537.36'})
-
-
+    
 def mil_seconds():
     return int(round(time() * 1000))
 
@@ -27,18 +26,20 @@ else:
     print('\nCurrently loaded: {}\n\n'.format(proxies))
 good_proxies, bad_proxies = [], []
 
-print('Testing on sites {}\n\n'.format(sites))
-
+print('Testing on sites {}\n\n'.format(sites))      
 
 def proxyChecker(proxy):
-    # session.proxies.update({
-    #     'http': 'http://' + proxy,
-    #     'https': 'https://' + proxy
-    # })
-    proxies = {
-        'http': 'http://' + proxy,
-        'https': 'https://' + proxy
-    }
+    try:
+        proxy_parts = proxy.split(':')
+        ip, port, user, passw = proxy_parts[0], proxy_parts[
+            1], proxy_parts[2], proxy_parts[3]
+        proxies = {
+            'http': 'http://{}:{}@{}:{}'.format(user, passw, ip, port),
+            'https': 'https://{}:{}@{}:{}'.format(user, passw, ip, port)
+        }
+    except IndexError:
+        proxies = {'http': 'http://:' + proxy, 'https': 'https://:' + proxy}
+
     for url in sites:
         start_time = mil_seconds()
         try:
@@ -59,10 +60,13 @@ if __name__ == '__main__':
     good_proxies_set, bad_proxies_set = set(good_proxies), set(bad_proxies)
 
     with open('proxy_results.txt', 'a') as proxy_results:
-        proxy_results.write('\n\nGood proxies: \n')
-        for proxy in good_proxies_set:
-            proxy_results.write('{}\n'.format(proxy))
-        proxy_results.write('Bad proxies: \n')
-        for proxy in bad_proxies_set:
-            proxy_results.write('{}\n'.format(proxy))
+        if good_proxies_set:
+            proxy_results.write('\n\nGood proxies: \n')
+            for proxy in good_proxies_set:
+                proxy_results.write('{}\n'.format(proxy))
+        if bad_proxies_set:
+            proxy_results.write('Bad proxies: \n')
+            for proxy in bad_proxies_set:
+                proxy_results.write('{}\n'.format(proxy))
+                
     print('\n\nGood proxies: {}'.format(good_proxies_set))
